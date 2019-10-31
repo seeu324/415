ccf7fdc6fee28595548b547871fe4eb44b9873ad
seeu324 <55227118+seeu324@users.noreply.github.com>
2019-10-31 14:50:00 +0800

    10/31

--- 414/Assets/Scripts/GameManager.cs
+++ 414/Assets/Scripts/GameManager.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio; //引用音頻apI
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     //定義欄位(宣告變數)
     //類型 名稱 結尾
     //public 公開、private 私人
     public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
 
     //定義方法 (宣告函式)
     //修飾詞 類型 名稱 (參數) { 敘述 }
@@ -15,7 +21,15 @@ public class GameManager : MonoBehaviour {
     {
         mixer.SetFloat("VBGM", v);
     }
-    
+
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
     }
-
-
+    
+    public void Play()
+    {
+        SceneManager.LoadScene("場景");
+    }
+        
+}
