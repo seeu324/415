0d5fcd691f61606c0eeb5bfd61fccd6e917b9854
seeu324 <55227118+seeu324@users.noreply.github.com>
2019-12-05 14:53:00 +0800

    1205

---
+++ 414/Assets/Scripts/暫停.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class 暫停 : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image  imagePause;
+    public Sprite spritPause, spritPLAY;
+
+    /// <summary>
+    /// 暫停方法
+    /// </summary>
+	// Use this for initialization
+	private void pause()
+    {
+
+    }
+    //更新:一秒執行約60次
+    private void Update()
+    {
+        pause();
+    }
+}
+
